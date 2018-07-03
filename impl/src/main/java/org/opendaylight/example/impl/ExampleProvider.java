49517f12bba577d680d2f44bdc821d49cec213d1
byeongkwanbae <bbangkwan@gmail.com>
2018-07-03 14:08:58 +0900

    rpc broker 예제 작성중

--- impl/src/main/java/org/opendaylight/example/impl/ExampleProvider.java
+++ impl/src/main/java/org/opendaylight/example/impl/ExampleProvider.java
@@ -7,18 +7,29 @@
  */
 package org.opendaylight.example.impl;
 
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.RpcRegistration;
 import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ProviderContext;
 import org.opendaylight.controller.sal.binding.api.BindingAwareProvider;
+import org.opendaylight.controller.sal.binding.api.RpcProviderRegistry;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.example.rev150105.ExampleService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ExampleProvider implements BindingAwareProvider, AutoCloseable {
 
     private static final Logger LOG = LoggerFactory.getLogger(ExampleProvider.class);
+    
+    private RpcProviderRegistry rpcProviderRegistry;
+    private RpcRegistration<ExampleService> exampleServiceRpcRegistration;
 
     @Override
     public void onSessionInitiated(ProviderContext session) {
         LOG.info("ExampleProvider Session Initiated");
+        
+        this.rpcProviderRegistry = session.getSALService(RpcProviderRegistry.class);
+        
+        exampleServiceRpcRegistration = session.addRpcImplementation(ExampleService.class, new ExampleImpl(this.rpcProviderRegistry));
     }
 
     @Override
---
+++ impl/src/main/yang-gen-sal/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/example/impl/rev141210/Example.java
@@ -0,0 +1,39 @@
+package org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.example.impl.rev141210;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.config.rev130405.ModuleType;
+import org.opendaylight.yangtools.yang.common.QName;
+
+
+/**
+ * &lt;p&gt;This class represents the following YANG schema fragment defined in module &lt;b&gt;example-impl&lt;/b&gt;
+ * &lt;br&gt;(Source path: &lt;i&gt;META-INF/yang/example-impl.yang&lt;/i&gt;):
+ * &lt;pre&gt;
+ * identity example {
+ *     base "()IdentitySchemaNodeImpl[base=null, qname=(urn:opendaylight:params:xml:ns:yang:controller:config?revision=2013-04-05)module-type]";
+ *     status CURRENT;
+ * }
+ * &lt;/pre&gt;
+ * The schema path to identify an instance is
+ * &lt;i&gt;example-impl/example&lt;/i&gt;
+ *
+ */
+public abstract class Example extends ModuleType
+ {
+    public static final QName QNAME = org.opendaylight.yangtools.yang.common.QName.cachedReference(org.opendaylight.yangtools.yang.common.QName.create("urn:opendaylight:params:xml:ns:yang:example:impl","2014-12-10","example"));
+
+
+    public Example() {
+    
+    
+    }
+    
+
+
+
+
+
+
+
+
+
+}
+
